c1e5150a231c3c57bea03b951e0a9889fe509d33
Erdnaell <leandre.roccaserra@gmail.com>
2026-02-19 15:24:54 +0100

    Update i18n.js

--- js/i18n.js
+++ js/i18n.js
@@ -352,7 +352,7 @@ const translations = {
     "cv.internships.research.M1.description1": " Génération de champs magnétiques quasi-statiques par interaction laser-matière avec des cibles à boucle",
     "cv.internships.research.M1.supervisor": "Encadré par Pr. Joao Santos.",
     "cv.internships.research.M1.description2": "Mesure de champs magnétiques par polarimétrie sur des images de tirs laser obtenues sur des expériences antérieures, participation à une campagne expérimentale sur le laser ABC à",
-    "cv.internships.research.M1.description3": "Frascati, en Italie et analyse préliminaire des images des premiers tirs",
+    "cv.internships.research.M1.description3": "Frascati, en Italie et analyse préliminaire des images des premiers tirs.",
     "cv.internships.research.L3.title": "Stage de L3",
     "cv.internships.research.L3.lab1": "Université de Stockholm",
     "cv.internships.research.L3.description.group.before": "groupe de recherche",
