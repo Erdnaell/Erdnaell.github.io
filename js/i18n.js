cedf8ef4b6682f53d429957dc0c2dd38035c2f89
Léandre Roccaserra <leandre.roccaserra@gmail.com>
2026-07-05 14:26:09 +0200

    small changes

--- js/i18n.js
+++ js/i18n.js
@@ -353,7 +353,7 @@ const translations = {
     
     "cv.education" : "Formation",
     "cv.education.master.title": "Master 1 Physique Fondamentale et Applications",
-    "cv.education.master.program": "Parcours Noyaux, Particules et Univers",
+    "cv.education.master.program": "Parcours Noyaux, Particules et Univers/Agrégation",
     "cv.education.master.school": "Université de Bordeaux",
     "cv.education.master.period": "2026",
 
